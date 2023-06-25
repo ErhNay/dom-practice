1a18a80783566e6272ab9fb136d41d90cc8050f5
ErhNay <erhnay86@gmail.com>
2023-06-25 12:19:37 +0000

    DOM (properties and methods-part 1)

---
+++ 2.DOM-properties-methods-1.js
@@ -0,0 +1,27 @@
+// // Property 1 => innerText
+
+// const para = document.querySelector('p');
+// console.log(para.innerText);
+
+// console.log('---------------------------------------------------');
+// // Property 2 => textContent
+// const para1 = document.querySelector('p');
+// console.log(para1.textContent);
+
+// // Property 3 => innerHTML
+// // example 1
+// const ul = document.querySelector('ul');
+// // console.log(ul.innerHTML);
+
+// // // example 2
+// // ul.innerHTML += '<ol><li>Hello</li><li>Hello</li></ol>';
+// // console.log(ul.innerHTML);
+
+// Method 1
+
+// const para = document.querySelector('p');
+// console.log(para.getAttribute('id'));
+
+// const para = document.querySelector('p');
+// para.setAttribute('class', 'alt-para');
+// console.log(para);
