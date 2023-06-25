389b14e12df6e0df2323ea6a73fc017863f577ae
ErhNay <erhnay86@gmail.com>
2023-06-25 12:49:51 +0000

    DOM (properties and methods-part 2)

---
+++ 3.DOM-properties-methods-2.js
@@ -0,0 +1,26 @@
+// Traversing the DOM with the following properties
+// parentElement
+// children
+// nextElementSibling
+// previousElementSibling
+
+// const para = document.querySelector('p');
+// console.log(para.parentElement);
+
+// const body = document.body;
+// console.log(body.children);
+// for (const child of body.children) {
+//   console.log(child);
+// }
+
+// const item = document.querySelector('ul li:nth-child(2)');
+// console.log(item.nextElementSibling);
+
+// const item1 = document.querySelector('ul li:nth-child(2)');
+// console.log(item.previousElementSibling);
+
+const img = document.querySelector('img');
+img.style.borderRadius = '50%';
+img.style.width = '300px';
+img.style.height = '300px';
+
