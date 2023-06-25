c28b636e4d44d2dd0a03ebd8d9cc853b052a9406
ErhNay <erhnay86@gmail.com>
2023-06-25 09:12:52 +0000

    Introduction to the DOM(Selection Methods)

---
+++ 1-intro-DOM.js
@@ -0,0 +1,11 @@
+//
+//*******************SELECTION METHODS ****************/
+// Type 1
+// const heading = document.getElementsByClassName('title')
+
+// Type 2
+// let dogImage = document.querySelector("#dog-img");
+
+// let dogTraits = document.querySelectorAll('.item');
+
+// console.log(dogTraits)
