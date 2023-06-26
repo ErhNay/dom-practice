4a4c39ddc754eb8c11349e19e8d1a6fbed0c278b
ErhNay <erhnay86@gmail.com>
2023-06-26 00:48:42 +0000

    DOM (properties and methods part -3)

---
+++ 4.DOM-properties-methods-3.js
@@ -0,0 +1,20 @@
+// const image = document.querySelector('img');
+// const para = document.querySelector('p');
+// console.log(image);
+// console.log(para);
+
+// // property => CLASSLIST
+
+// para.classList.add('for-fun');
+
+// image.style.height = '600px';
+// image.style.width = '800px';
+
+// const imageProps = getComputedStyle(image);
+// console.log(imageProps);
+
+// console.log(imageProps.height);
+// console.log(imageProps.width);
+
+// setTimeout(() => image.classList.remove('for-fun'), 1000);
+
