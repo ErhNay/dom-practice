2922c158e085519d5f02e37b2dbafe3d7591e0f1
ErhNay <erhnay86@gmail.com>
2023-06-26 03:35:13 +0000

    DOM-events-part-2

--- 8.DOM-events-part-1.js
+++ 8.DOM-events-part-1.js
@@ -13,12 +13,38 @@ user presses key, etc
 const changeBtn = document.querySelector('.change-bg');
 const resetBtn = document.querySelector('.reset-bg');
 
-changeBtn.addEventListener('click', function () {
-  document.body.style.backgroundColor = '#b23c3e';
-  console.log('The Change button has been pressed');
-});
+// changeBtn.addEventListener('click', function () {
+//   document.body.style.backgroundColor = '#b23c3e';
+//   console.log('The Change button has been pressed');
+// });
 
-resetBtn.addEventListener('click', function () {
-  document.body.style.backgroundColor = '#ffff';
-  console.log('The Reset button has been pressed');
-});
+// resetBtn.addEventListener('click', function () {
+//   document.body.style.backgroundColor = '#ffff';
+//   console.log('The Reset button has been pressed');
+// });
+
+// changeBtn.addEventListener('dblclick', function () {
+//   document.body.style.backgroundColor = '#c6733e';
+//   console.log('The Change button has been pressed');
+// });
+
+// resetBtn.addEventListener('dblclick', function () {
+//   document.body.style.backgroundColor = '#ffff';
+//   console.log('The Reset button has been pressed');
+// });
+
+// changeBtn.addEventListener('mouseover', function () {
+//   document.body.style.backgroundColor = '#fd3a43';
+//   console.log('The Change button has been pressed');
+// });
+
+// resetBtn.addEventListener('mouseout', function () {
+//   document.body.style.backgroundColor = 'teal';
+//   console.log('The Change button has been pressed');
+// });
+
+// window.addEventListener('scroll', function () {
+//   document.body.style.lineHeight = 2.1;
+//   document.body.style.backgroundColor = 'navy';
+//   document.body.style.color = 'whitesmoke';
+// });
